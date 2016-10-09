3d56db6e40410dd701b612b0d78bd8bf31c1131b
Mike Faraponov <java.math.random@bk.ru>
2016-10-09 09:29:26 +0000

    Renamed module because of name conventions

---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+from facelook import app 
--- app/facelook.py
+++ app/facelook.py
@@ -1,12 +1,12 @@
 import os
 import cv2
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    """Return rendered home page template"""
+def index():
+    """Return rendered index page template"""
     return render_template('index.html', items=range(100))
 
 @app.route('/detect', methods=['POST'])
@@ -15,12 +15,8 @@ def detect():
     Detect face on the photo and 
     return json with the result of presence/abcense of face analyze
     """
-    return jsonify(dict(result=True))
+    return json.dumps(dict(result=True))
 
 @app.errorhandler(404)
 def not_found(err):
     return render_template('404.html'), 404
-
-if __name__ == '__main__':
-    OPTIONS = dict(debug=1,host='0.0.0.0')
-    app.run(**OPTIONS)
