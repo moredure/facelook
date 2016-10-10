8f8adb9a56803b516a31db9ef74ebda8a7f62851
Mike Faraponov <java.math.random@bk.ru>
2016-10-10 13:38:05 +0000

    Added client js application development environment and vital modules

---
+++ client/app/index.js
@@ -0,0 +1,5 @@
+const styles = require('./css/style.scss');
+const $ = require('jquery');
+const Rx = require('rxjs');
+
+console.log('Hello, World!');
---
+++ client/webpack.config.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, '/app/index.js'),
+    output: {
+        path: path.join(__dirname, '..', 'app/static/javascript'),
+        filename: 'bundle.js'
+    },
+    devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
+    watch: NODE_ENV == 'development',
+    watchOptions: {
+        poll: true
+    },
+    resolve: {
+        extensions: ['', '.js', '.scss'],
+        fallback: path.join(__dirname, 'node_modules')
+    },
+    resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+    },
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style-loader', 
+                `css${NODE_ENV == 'development'? '': '?minimize'}!sass`)
+        },
+        {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 
+                `css${NODE_ENV == 'development'? '': '?minimize'}`)
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin('../css/style.css'),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ]
+};
+
+if(NODE_ENV == 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            },
+            output: {
+                comments: false
+            }
+        })
+    );
+}
