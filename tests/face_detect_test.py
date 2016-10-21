4816be4dc3d76466fa21976d0e0ba072c026fa43
wildkun <a7244271>
2016-10-21 06:32:45 +0300

    UnitTest, needs a fix

--- tests/face_detect_test.py
+++ tests/face_detect_test.py
@@ -1,14 +1,29 @@
 import os
 import sys
 import unittest
-from app import app
+import StringIO
+from app import  app
 from flask import jsonify, json
+import tempfile #check if needed
+from contextlib import contextmanager
+from flask import appcontext_pushed
 
 class DetectFaceTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def detect_face_test(self):
+    def test_test(self):
         pass
 
+    def detect (self):
+        with open('tests/testimages/1.jpg') as test:
+            imgStringIO = StringIO(test.read())
+        return self.app.post('/detect', content_type='multipart/form-data', data=dict(
+            {'file': (imgStringIO, '1.jpg')}
+            ))
 
+    def test_detect(self):
+        rv = self.detect()
+        assert dict(error='Unsupported extension!') in rv.data
+
+#Doesn't work, needs a fix
