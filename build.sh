3d66f961a59a2e7e8c451faac5cbaf022d0d9317
m.faraponov <mikefaraponov@gmail.com>
2016-10-23 15:51:22 +0300

    Added docker stop/rm for cleaning containers before build

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 cd client && npm run build
 if [ $? -eq 0 ]; then
 	cd ..
+	docker stop $(docker ps -a -q)
+	docker rm $(docker ps -a -q)
 	docker rmi mikefaraponov/facelook:latest
 	docker build . -t mikefaraponov/facebook:latest
 fi
