802430cb5396ce6024166a66c8436347aff523f6
wildkun <a7244271>
2016-10-24 17:54:23 +0300

    Deleted unnesessary convertion to greyscale Working on fixing error when files are too big

--- app/facelook.py
+++ app/facelook.py
@@ -1,5 +1,10 @@
 import cv2
 import imghdr
+import os
+
+import sys
+import time
+
 from os import path, environ
 from numpy import asarray, uint8
 from flask import Flask, request, \
@@ -10,7 +15,7 @@ from flask_jsontools import jsonapi
 ROOT_PATH = path.dirname(__file__)
 CASCADE_PATH = path.abspath(ROOT_PATH) + \
     '/haarcascade_frontalface_default.xml'
-WHITELIST = ['png', 'jpeg', 'gif']
+WHITELIST = ['png', 'jpeg']
 
 application = Flask(__name__)
 application.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
@@ -28,14 +33,22 @@ def detect():
     Content-Type: application/json
     list{list{x1, y1, width, height}}
     """
+    # try:
     file = request.files['file']
+    # print (type(file))
+    # if type(file) is not FileStorage:
+    #     return dict(error='File is too big!'), 420
+    # file.seek(0, os.SEEK_END)
+    # print(file.tell())
+    # file.seek(0,0)
+    # # if file.tell() > 1000000:
+    # #     return dict(error='File is too big!'), 420
     if imghdr.what(file) not in WHITELIST:
         return dict(error='Unsupported extension!'), 415
     arr = asarray(bytearray(file.read()), dtype=uint8)
-    img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+    img = cv2.imdecode(arr, cv2.IMREAD_GRAYSCALE)
     detector = cv2.CascadeClassifier(CASCADE_PATH)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detector.detectMultiScale(gray, scaleFactor=1.15, minNeighbors=6,
+    faces = detector.detectMultiScale(img, scaleFactor=1.15, minNeighbors=6,
             minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
     return faces.tolist() if len(faces) else []
 
