bf5c0de0f40a21d31a8526c2455f0d2934486120
m.faraponov <mikefaraponov@gmail.com>
2016-10-25 10:23:52 +0300

    Added unit tests and refactored api method

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,19 +1,26 @@
 import {DOM} from 'rx-dom';
 import {FACE_DETECTION_ENDPOINT} from '../config';
+const {post, fromReader} = DOM;
 
-/**
- * Call to /detect API endpoint to detect faces on an image
- * @param  {File} file image to upload
- * @return {Observable} ajax observable
- */
-export function faceDetectionAPI(file) {
+export const fileAsDataURI$ = file => fromReader(file).asDataURL();
+
+export const detectFaceCall$ = file => {
   let form = new FormData();
   form.append('file', file);
-  const detectFaceCall$ = DOM.post({
+  return post({
     url: FACE_DETECTION_ENDPOINT,
     responseType: 'json',
     body: form
   });
-  const fileAsDataURI$ = DOM.fromReader(file).asDataURL();
-  return detectFaceCall$.concat(fileAsDataURI$);
+};
+
+/**
+ * Call to /detect API endpoint to detect faces on an image and
+ * concat to the result file asDataUri
+ * @param  {File} file image to upload
+ * @return {Observable} ajax observable
+ */
+export function faceDetectionAPI(file) {
+  return detectFaceCall$(file)
+    .concat(fileAsDataURI$(file));
 }
--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
 export const API_ENDPOINT = '/api';
-export const EXTENSIONS_WHITELIST = ['png', 'jpeg', 'gif'];
+export const EXTENSIONS_WHITELIST = ['png', 'jpeg'];
 export const FACE_DETECTION_ENDPOINT = `${API_ENDPOINT}/detect`;
 export const MAX_FILE_SIZE = 1 * 1024 * 1024;
